5225c0465053156d502e7f0805ac5277582aa9fb
sujalkyal <sujalkyal2704@gmail.com>
2025-02-24 16:07:55 +0530

    added collection route

--- apps/user/app/api/product/collection/route.js
+++ apps/user/app/api/product/collection/route.js
@@ -1,1 +1,23 @@
 // returns products according to category
+
+import { NextResponse } from "next/server";
+import prisma from "@repo/db/client";
+
+export async function GET(req) {
+    try {
+        const { category } = await req.json();
+        const products = await prisma.product.findMany({
+            where: {
+                category: category,
+            },
+        });
+
+        if (!products) {
+            return NextResponse.json({ message: "Products not found" }, { status: 404 });
+        }
+
+        return NextResponse.json(products);
+    } catch (error) {
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+}
