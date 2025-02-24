93a23c7c30d9b13261ab1494e65d4ec5ffa538b4
Rishav Banerjee <therealrishav@gmail.com>
2025-02-24 16:07:52 +0530

    added Wishlist Routes

---
+++ apps/user/app/api/user/wishlist/removeItem/route.js
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import prisma from "@repo/db/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../lib/auth";
+
+export async function POST(req) {
+    try{
+        const session = await getServerSession(authOptions);
+        if(!session) {
+            return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+        }
+        const id = session.user.id;
+        const [productId] = await req.json()
+        const user = await prisma.user.findFirst({
+            where: {
+                id: id,
+            },
+            select: {
+                wishlist: true
+            }
+        });
+    
+        if (!user.wishlist) {
+            return NextResponse.json({ message: "Wishlist not found" }, { status: 404 });
+        }
+
+        const updatedWishlist = await prisma.user.update({
+            where: { id },
+            data: {
+                wishlist: {
+                    set: user.wishlist.filter((item) => item !== productId)
+                }
+            }
+        });
+
+        return NextResponse.json(updatedWishlist);
+    } catch (error) {
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+}
