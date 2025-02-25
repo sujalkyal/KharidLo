c5b2ef1894b73a5456a9f105cff437d1145cd298
sujalkyal <sujalkyal2704@gmail.com>
2025-02-25 16:33:43 +0530

    tailwind-initialised

--- apps/user/app/page.tsx
+++ apps/user/app/page.tsx
@@ -1,10 +1,15 @@
+"use client"
+
+import ProductCard from '../components/Card';
 import React from 'react';
+import './globals.css';
 
 export default function Home() {
 
   return (
     <div>
       this is user page...
+      <ProductCard/>
     </div>
   );
 }
