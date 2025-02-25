bcc32dd5bffd006dc7ca8f14e0344b1b4ee424c0
sujalkyal <sujalkyal2704@gmail.com>
2025-02-26 01:58:58 +0530

    fake

--- apps/user/app/(dashboard)/cart/page.js
+++ apps/user/app/(dashboard)/cart/page.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import axios from "axios";
-import CartItem from "../../../components/CartItem";
+import CartItem from "../components/CartItem";
 
 const CartPage = () => {
   const [cart, setCart] = useState([]);
